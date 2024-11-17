2bf32b57fd14062e4e72487d6b91ee4ea01e8a8b
Hariom-vishwa <hawkgamerhv@gmail.com>
2024-11-17 20:34:27 +0530

    Hamburger

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// Hamburger
+const hamMenu = document.querySelector(".hamMenu")
+
+function openHam() {
+  const openHamElement = document.querySelector(".openHam");
+  const closeHamElement = document.querySelector(".closeHam");
+
+    openHamElement.style.display = "none";
+    closeHamElement.style.display = "block";
+    hamMenu.style.display = "flex"
+
+    hamMenu.innerHTML = `
+  <a href="#">Home</a>
+  <a href="#">Dropdown</a>
+  <a href="#">Dropdown</a>
+  <a href="#">Dropdown</a>
+  <a href="#">About Us</a>
+    `
+
+}
+
+function closeHam(){
+  const openHamElement = document.querySelector(".openHam");
+  const closeHamElement = document.querySelector(".closeHam");
+  openHamElement.style.display = "block"
+  closeHamElement.style.display = "none"
+  hamMenu.style.display = "none"
+
+
+}
+
+
+
 // Carousel
 
 let slideIndex = 1;
@@ -30,4 +63,6 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+// Carousel End
 
