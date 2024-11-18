7fcc41105e7d27136097fff010e167cc4f268805
Hariom-vishwa <hawkgamerhv@gmail.com>
2024-11-18 22:12:13 +0530

    Facilities

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Hamburger
+
 const hamMenu = document.querySelector(".hamMenu")
 
 function openHam() {
@@ -47,7 +48,6 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -57,11 +57,8 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
 
 // Carousel End
