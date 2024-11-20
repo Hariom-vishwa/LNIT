e446b78502830c1ab94fc9168f429e967e5b0210
Hariom-vishwa <hawkgamerhv@gmail.com>
2024-11-20 09:15:03 +0530

    header and footer separation

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-// Hamburger
-
-const hamMenu = document.querySelector(".hamMenu")
-
-function openHam() {
-  const openHamElement = document.querySelector(".openHam");
-  const closeHamElement = document.querySelector(".closeHam");
-
-    openHamElement.style.display = "none";
-    closeHamElement.style.display = "block";
-    hamMenu.style.display = "flex"
-
-    hamMenu.innerHTML = `
-  <a href="#">Home</a>
-  <a href="#">Dropdown</a>
-  <a href="#">Dropdown</a>
-  <a href="#">Dropdown</a>
-  <a href="#">About Us</a>
-    `
-
-}
-
-function closeHam(){
-  const openHamElement = document.querySelector(".openHam");
-  const closeHamElement = document.querySelector(".closeHam");
-  openHamElement.style.display = "block"
-  closeHamElement.style.display = "none"
-  hamMenu.style.display = "none"
-
-
-}
-
 
 
 // Carousel
